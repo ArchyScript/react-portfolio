59b38c93384c83148bfd59ad82f304fbcf0657a2
ArchyScript <yungdansa@gmail.com>
2022-08-07 13:21:31 +0100

    ...

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -35,9 +35,20 @@ const Projects = () => {
                 </div>
 
                 <div className="z-4 flex justify-center">
-                  <div className="w-14 sm:w-20 h-14 sm:h-20 -mt-6 sm:-mt-12 p-4 bg-white flex justify-center items-center cursor-pointer rounded-full">
-                    <span className="fa fa-share-square bg-gray-50 p-2 sm:p-4 rounded-full text-indigo-600 text-2xl sm:text-3xl  shadow-md hover:shadow-indigo-600"></span>
-                  </div>
+                  <a
+                    href={project.links.netlify}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="w-14 sm:w-20 h-14 sm:h-20 -mt-6 sm:-mt-12 p-2 bg-white flex justify-center items-center cursor-pointer rounded-full"
+                  >
+                    <svg
+                      className="fill-indigo-600 p-2  rounded-full shadow-md hover:shadow-indigo-600"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 576 512"
+                    >
+                      <path d="M568.9 143.5l-150.9-138.2C404.8-6.773 384 3.039 384 21.84V96C241.2 97.63 128 126.1 128 260.6c0 54.3 35.2 108.1 74.08 136.2c12.14 8.781 29.42-2.238 24.94-16.46C186.7 252.2 256 224 384 223.1v74.2c0 18.82 20.84 28.59 34.02 16.51l150.9-138.2C578.4 167.8 578.4 152.2 568.9 143.5zM416 384c-17.67 0-32 14.33-32 32v31.1l-320-.0013V128h32c17.67 0 32-14.32 32-32S113.7 64 96 64H64C28.65 64 0 92.65 0 128v319.1c0 35.34 28.65 64 64 64l320-.0013c35.35 0 64-28.66 64-64V416C448 398.3 433.7 384 416 384z" />
+                    </svg>
+                  </a>
                 </div>
 
                 <div className="-mt-2">
